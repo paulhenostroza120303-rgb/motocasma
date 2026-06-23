d9d0df348721002f077b675b3a69c563e6aafc72
paulhenostroza120303-rgb <paulhenostroza120303@gmail.com>
2026-06-22 20:07:36 -0500

    Set Firebase config with real project credentials

--- www/js/config.js
+++ www/js/config.js
@@ -2,9 +2,12 @@ const CONFIG = {
   API_URL: 'https://motocasma-production.up.railway.app',
   SOCKET_URL: 'https://motocasma-production.up.railway.app',
   FIREBASE: {
-    apiKey: 'REEMPLAZA_CON_TU_API_KEY',
-    authDomain: 'REEMPLAZA.firebaseapp.com',
-    projectId: 'REEMPLAZA',
-    appId: '1:XXXXXXXXXX:web:XXXXXXXXXX'
+    apiKey: 'AIzaSyA2EMrjAkuuff5DwJFxDfUGNguzBi4_N5s',
+    authDomain: 'motocasma-c9ad5.firebaseapp.com',
+    projectId: 'motocasma-c9ad5',
+    storageBucket: 'motocasma-c9ad5.firebasestorage.app',
+    messagingSenderId: '291394513768',
+    appId: '1:291394513768:web:769967e4763d8c98ac84dd',
+    measurementId: 'G-BXEE0NV05M'
   }
 };
